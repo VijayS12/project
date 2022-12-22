24fc9e1af4e101055c380dfdecacec5cfe8adcd9
vijay sonulkar <you@example.comvijay.sonulkar@gmail.com>
2022-12-22 19:47:55 +0530

    program added succesfully

---
+++ Biggest_no.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Biggest_no {
+	// which one is biggest tried form if else if, else
+		public static void main(String[] args) {
+			int a=6,b=9,c=10;
+			if(a>b&&a>c)
+	System.out.println(a+" is biggest");
+			else if (b>a&&b>c)
+				System.out.println(b+" is biggest");
+			else System.out.println(c+" is biggest");	
+		}
+}
